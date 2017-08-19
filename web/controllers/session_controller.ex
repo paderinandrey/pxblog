d5de36e677a8ad367e39b8cc7ddd768caa57ada9
Andrey <paderinandrey2011@gmail.com>
2017-08-19 23:25:16 +0000

    fix user sessions

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -3,7 +3,7 @@ defmodule Pxblog.SessionController do
 
   alias Pxblog.User
 
-  import Comeonin.Bcrypt, only: [checkpw: 2]
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 
   plug :scrub_params, "user" when action in [:create]
 
@@ -11,9 +11,13 @@ defmodule Pxblog.SessionController do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-    Repo.get_by(User, username: user_params["username"])
-    |> sign_in(user_params["password"], conn)
+  def create(conn, %{"user" => %{"user_params" => username, "password" => password}}) when not is_nil(username) and not is_nil(password) do
+    user = Repo.get_by(User, username: username)
+    sign_in(user, password, conn)
+  end
+
+  def create(conn, _) do
+    failed_login(conn)
   end
 
   def delete(conn, _params) do
@@ -24,9 +28,7 @@ defmodule Pxblog.SessionController do
   end
 
   defp sign_in(user, _password, conn) when is_nil(user) do
-    conn
-    |> put_flash(:error, "Invalid username/password combination!")
-    |> redirect(to: page_path(conn, :index))
+    failed_login(conn)
   end
 
   defp sign_in(user, password, conn) do
@@ -36,10 +38,16 @@ defmodule Pxblog.SessionController do
       |> put_flash(:info, "Sign in successfull!")
       |> redirect(to: page_path(conn, :index))
     else
-      conn
-      |> put_session(:current_user, nil)
-      |> put_flash(:error, "Invalid username/password combination!")
-      |> redirect(to: page_path(conn, :index))
+      failed_login(conn)
     end
+  end
+
+  defp failed_login(conn) do
+    dummy_checkpw()
+    conn
+    |> put_session(:current_user, nil)
+    |> put_flash(:error, "Invalid username/password combination!")
+    |> redirect(to: page_path(conn, :index))
+    |> halt()
   end
 end
